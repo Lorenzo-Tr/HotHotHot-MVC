63f1b67d345353672576ebf4596914245d199e94
Lorenzo.T <59581743+Lorenzo-Tr@users.noreply.github.com>
2021-12-01 17:09:46 +0100

    refractor changed file

--- kernel/Providers/Constants.php
+++ kernel/Providers/Constants.php
@@ -6,9 +6,3 @@ class Constants{
         return APP_PATH . "/resources/views/";
     }
 }
-
-if($_SERVER["SERVER_PORT"] === "443"){
-    define('WEB_URL', $_SERVER["HTTP_REFERER"]);
-}elseif ($_SERVER["SERVER_PORT"] === "80"){
-    define('WEB_URL', "http://".$_SERVER["SERVER_NAME"]."/");
-}
