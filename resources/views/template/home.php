961dbaa592d86eac0f17325d9d3164850e5b09c6
Lorenzo.T <59581743+Lorenzo-Tr@users.noreply.github.com>
2022-02-07 10:16:13 +0100

    webmanifest & sw

--- resources/views/template/home.php
+++ resources/views/template/home.php
@@ -1,12 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/resources/assets/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/resources/assets/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/resources/assets/favicon/favicon-16x16.png">
+        <link rel="manifest" href="/public/site.webmanifest">
+        <link rel="mask-icon" href="/resources/assets/favicon/safari-pinned-tab.svg" color="#56ce7f">
+        <meta name="msapplication-TileColor" content="#00a300">
+        <meta name="theme-color" content="#56ce7f">
+
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><?= $title?></title>
         <link rel="stylesheet" href="/resources/css/home.css">
         <script type="module" src="/resources/js/home.js" ></script>
+        <script src="/resources/js/app.js"></script>
     </head>
     <body class="theme-light">
         <?php section('content')?>
